30b3af197b243581a57e1507d2b4516d285f8ad1
Iain <25081046+Iain-S@users.noreply.github.com>
2022-09-14 00:52:36 +0100

    Add opencv

--- sentrybot/__init__.py
+++ sentrybot/__init__.py
@@ -1,11 +1,40 @@
 """An autonomous sentry turret."""
-import os
-from typing import Final, Mapping, Optional
+import logging
+from pathlib import Path
+from typing import Final, Generator, Mapping, Optional
 
+import cv2
 import toml
-from flask import Flask
+from flask import Flask, Response, render_template
 
 __version__: Final = "0.1.0"
+
+
+def generate_video(video_path: str) -> Generator[bytes, None, None]:
+    """Generate a video stream."""
+    # pylint: disable=no-member
+    while True:
+        cap = cv2.VideoCapture(video_path)
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                logging.warning("Can't receive frame (stream end?).")
+                break
+
+            # Adjust for best performance
+            frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
+
+            # Encode the frame in JPEG format
+            (flag, encoded_image) = cv2.imencode(".jpg", frame)
+            # Ensure the frame was successfully encoded
+            if flag:
+                yield (
+                    b"--frame\r\n"
+                    b"Content-Type: image/jpeg\r\n\r\n"
+                    + bytearray(encoded_image)
+                    + b"\r\n"
+                )
 
 
 def create_app(test_config: Optional[Mapping] = None) -> Flask:
@@ -13,25 +42,41 @@ def create_app(test_config: Optional[Mapping] = None) -> Flask:
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        DATABASE=Path(app.instance_path) / "flaskr.sqlite",
     )
+
+    # ensure the instance folder exists
+    Path(app.instance_path).mkdir(parents=True, exist_ok=True)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_file("config.toml", load=toml.load, silent=True)
+        app.config.from_file(
+            str(Path(app.instance_path) / "config.toml"),
+            load=toml.load,
+            # silent=True
+        )
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
     @app.route("/hello")
     def hello() -> str:
+        # a simple page that says hello
         return "Hello, World!"
+
+    @app.route("/")
+    def index() -> str:
+        # return the rendered template
+        return render_template("index.html")
+
+    @app.route("/video_feed")
+    def video_feed() -> Response:
+        # return the response generated along with the specific media
+        # type (mime type)
+        video_path = app.config["VIDEO_PATH"]
+        return Response(
+            generate_video(video_path),
+            mimetype="multipart/x-mixed-replace; boundary=frame",
+        )
 
     return app
