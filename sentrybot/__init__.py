7b6996f90a93a8a760cb55ab52c997f00b36fcc3
Iain <25081046+Iain-S@users.noreply.github.com>
2022-09-13 22:39:35 +0100

    Add flask

--- sentrybot/__init__.py
+++ sentrybot/__init__.py
@@ -1,2 +1,37 @@
 """An autonomous sentry turret."""
-__version__ = "0.1.0"
+import os
+from typing import Final, Mapping, Optional
+
+import toml
+from flask import Flask
+
+__version__: Final = "0.1.0"
+
+
+def create_app(test_config: Optional[Mapping] = None) -> Flask:
+    """Create and configure the app."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_file("config.toml", load=toml.load, silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello() -> str:
+        return "Hello, World!"
+
+    return app
