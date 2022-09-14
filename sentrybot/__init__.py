27f5b1d9a0e739dbd0c7446cf6a345f459d7780e
Iain <25081046+Iain-S@users.noreply.github.com>
2022-09-14 18:53:43 +0100

    Add face detection

--- sentrybot/__init__.py
+++ sentrybot/__init__.py
@@ -1,6 +1,7 @@
 """An autonomous sentry turret."""
 import logging
 from pathlib import Path
+from time import sleep
 from typing import Final, Generator, Mapping, Optional
 
 import cv2
@@ -11,7 +12,7 @@ __version__: Final = "0.1.0"
 
 
 def generate_video(video_path: str) -> Generator[bytes, None, None]:
-    """Generate a video stream."""
+    """Generate a video stream from a file."""
     # pylint: disable=no-member
     while True:
         cap = cv2.VideoCapture(video_path)
@@ -27,6 +28,7 @@ def generate_video(video_path: str) -> Generator[bytes, None, None]:
 
             # Encode the frame in JPEG format
             (flag, encoded_image) = cv2.imencode(".jpg", frame)
+
             # Ensure the frame was successfully encoded
             if flag:
                 yield (
@@ -35,6 +37,49 @@ def generate_video(video_path: str) -> Generator[bytes, None, None]:
                     + bytearray(encoded_image)
                     + b"\r\n"
                 )
+
+
+def generate_face_detection_video() -> Generator[bytes, None, None]:
+    """Generate a video stream from a camera, with face detection rectangles."""
+    # pylint: disable=no-member,invalid-name
+
+    # cv2 comes with cascade files
+    casc_path = Path(cv2.__path__[0]) / "data/haarcascade_frontalface_default.xml"
+    face_cascade = cv2.CascadeClassifier(str(casc_path))
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        if not ret:
+            logging.warning("Can't receive frame (stream end?).")
+            sleep(0.03)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE,
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Encode the frame in JPEG format
+        (flag, encoded_image) = cv2.imencode(".jpg", frame)
+
+        # Ensure the frame was successfully encoded
+        if flag:
+            yield (
+                b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encoded_image) + b"\r\n"
+            )
 
 
 def create_app(test_config: Optional[Mapping] = None) -> Flask:
@@ -62,12 +107,24 @@ def create_app(test_config: Optional[Mapping] = None) -> Flask:
         # a simple page that says hello
         return "Hello, World!"
 
+    @app.route("/face-detection")
+    def face_detection() -> str:
+        # return the rendered template
+        return render_template("face_detection.html")
+
+    @app.route("/face-detected-video-feed")
+    def face_detected_video() -> Response:
+        return Response(
+            generate_face_detection_video(),
+            mimetype="multipart/x-mixed-replace; boundary=frame",
+        )
+
     @app.route("/")
     def index() -> str:
         # return the rendered template
         return render_template("index.html")
 
-    @app.route("/video_feed")
+    @app.route("/video-feed")
     def video_feed() -> Response:
         # return the response generated along with the specific media
         # type (mime type)
