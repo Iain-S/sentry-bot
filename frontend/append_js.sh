b223a17fdd7b8e1356b72551fb4e071117bbaf43
Iain <25081046+Iain-S@users.noreply.github.com>
2022-09-16 14:55:29 +0100

    Work around GitHub action's sed

--- frontend/append_js.sh
+++ frontend/append_js.sh
@@ -8,4 +8,7 @@ set -e
 # import { sum } from "./sum";
 # with
 # import { sum } from "./sum.js";
-sed -E -i '' 's/(from \".\/[a-z]+)(\";)/\1.js\2/g' ../sentrybot/static/js/*.js
+# See also https://stackoverflow.com/a/22084103/3324095
+sed -i.backup \
+    -r 's/(from \".\/[a-z]+)(\";)/\1.js\2/g' \
+    ../sentrybot/static/js/*.js
