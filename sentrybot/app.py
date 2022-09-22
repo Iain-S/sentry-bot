21d09d6ededb9886d23e0cdc9621ab1ee59cf539
Iain <25081046+Iain-S@users.noreply.github.com>
2022-09-22 17:06:31 +0100

    Move video generators to their own module

--- sentrybot/app.py
+++ sentrybot/app.py
@@ -1,98 +1,11 @@
 """App factory module."""
-import logging
 from pathlib import Path
-from time import sleep
-from typing import Generator, List, Mapping, Optional
+from typing import List, Mapping, Optional
 
-import cv2
 import toml
 from flask import Flask, Response, abort, render_template, request, send_from_directory
 
-
-def generate_video(video_path: str) -> Generator[bytes, None, None]:
-    """Generate a video stream from a file."""
-    # pylint: disable=no-member
-    while True:
-        cap = cv2.VideoCapture(video_path)
-
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                logging.warning("Can't receive frame (stream end?).")
-                break
-
-            # Adjust for best performance
-            frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
-
-            # Encode the frame in JPEG format
-            (flag, encoded_image) = cv2.imencode(".jpg", frame)
-
-            # Ensure the frame was successfully encoded
-            if flag:
-                yield (
-                    b"--frame\r\n"
-                    b"Content-Type: image/jpeg\r\n\r\n"
-                    + bytearray(encoded_image)
-                    + b"\r\n"
-                )
-
-
-def generate_face_detection_video(
-    mouse_position: List[int],
-) -> Generator[bytes, None, None]:
-    """Generate a video stream from a camera, with face detection rectangles."""
-    # pylint: disable=no-member,invalid-name,too-many-locals
-
-    # cv2 comes with cascade files
-    casc_path = Path(cv2.__path__[0]) / "data/haarcascade_frontalface_default.xml"
-    face_cascade = cv2.CascadeClassifier(str(casc_path))
-
-    video_capture = cv2.VideoCapture(0)
-
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
-
-        if not ret:
-            logging.warning("Can't receive frame (stream end?).")
-            sleep(0.03)
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = face_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE,
-        )
-
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Draw a dot where the mouse is
-        if mouse_position:
-            mouse_x = mouse_position[0]
-            mouse_y = mouse_position[1]
-            logging.debug("x:%s  y:%s", mouse_x, mouse_y)
-            cv2.rectangle(
-                frame,
-                (mouse_x, mouse_y),
-                (mouse_x + 10, mouse_y + 10),
-                (255, 0, 0),
-                4,
-            )
-
-        # Encode the frame in JPEG format
-        (flag, encoded_image) = cv2.imencode(".jpg", frame)
-
-        # Ensure the frame was successfully encoded
-        if flag:
-            yield (
-                b"--frame\r\n"
-                b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encoded_image) + b"\r\n"
-            )
+from sentrybot.video import generate_face_detection_video, generate_video
 
 
 def create_app(test_config: Optional[Mapping] = None) -> Flask:
@@ -131,13 +44,10 @@ def create_app(test_config: Optional[Mapping] = None) -> Flask:
 
     @app.route("/")
     def index() -> str:
-        # return the rendered template
         return render_template("index.html")
 
     @app.route("/video-feed")
     def video_feed() -> Response:
-        # return the response generated along with the specific media
-        # type (mime type)
         video_path = app.config["VIDEO_PATH"]
         return Response(
             generate_video(video_path),
@@ -146,7 +56,6 @@ def create_app(test_config: Optional[Mapping] = None) -> Flask:
 
     @app.route("/send-receive")
     def send_receive() -> str:
-        # Return a Dict, which will be jsonified automatically
         return render_template("send_receive.html")
 
     @app.route("/ajax-data", methods=["POST", "GET"])
@@ -161,7 +70,7 @@ def create_app(test_config: Optional[Mapping] = None) -> Flask:
 
             abort(404, description="One or more of xPos and yPos are missing")
         else:
-            # Return a Dict, which will be jsonified automatically
+            # Return a dict, which will be jsonified automatically
             return {"aString": "hello, world", "aNumber": 55}
 
     @app.route("/game")
---
+++ sentrybot/video.py
@@ -0,0 +1,93 @@
+"""Generators for video streams."""
+import logging
+from pathlib import Path
+from time import sleep
+from typing import Generator, List
+
+import cv2
+
+
+def generate_video(video_path: str) -> Generator[bytes, None, None]:
+    """Generate a video stream from a file."""
+    # pylint: disable=no-member
+    while True:
+        cap = cv2.VideoCapture(video_path)
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                logging.warning("Can't receive frame (stream end?).")
+                break
+
+            # Adjust for best performance
+            frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
+
+            # Encode the frame in JPEG format
+            (flag, encoded_image) = cv2.imencode(".jpg", frame)
+
+            # Ensure the frame was successfully encoded
+            if flag:
+                yield (
+                    b"--frame\r\n"
+                    b"Content-Type: image/jpeg\r\n\r\n"
+                    + bytearray(encoded_image)
+                    + b"\r\n"
+                )
+
+
+def generate_face_detection_video(
+    mouse_position: List[int],
+) -> Generator[bytes, None, None]:
+    """Generate a video stream from a camera, with face detection rectangles."""
+    # pylint: disable=no-member,invalid-name,too-many-locals
+
+    # cv2 comes with cascade files
+    casc_path = Path(cv2.__path__[0]) / "data/haarcascade_frontalface_default.xml"
+    face_cascade = cv2.CascadeClassifier(str(casc_path))
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        if not ret:
+            logging.warning("Can't receive frame (stream end?).")
+            sleep(0.03)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE,
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Draw a dot where the mouse is
+        if mouse_position:
+            mouse_x = mouse_position[0]
+            mouse_y = mouse_position[1]
+            logging.debug("x:%s  y:%s", mouse_x, mouse_y)
+            cv2.rectangle(
+                frame,
+                (mouse_x, mouse_y),
+                (mouse_x + 10, mouse_y + 10),
+                (255, 0, 0),
+                4,
+            )
+
+        # Encode the frame in JPEG format
+        (flag, encoded_image) = cv2.imencode(".jpg", frame)
+
+        # Ensure the frame was successfully encoded
+        if flag:
+            yield (
+                b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encoded_image) + b"\r\n"
+            )
