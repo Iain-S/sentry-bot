17af0f49e830f80a2213062b12f015305a795150
Iain <25081046+Iain-S@users.noreply.github.com>
2022-09-25 23:27:52 +0100

    Switch from PWMDevice to Servo

--- tests/test_turret_controller.py
+++ tests/test_turret_controller.py
@@ -1,5 +1,5 @@
 """Tests for the turret_controller module."""
-from unittest.mock import patch
+from unittest.mock import MagicMock, call, patch
 
 import pytest
 
@@ -10,17 +10,11 @@ def test_turret_controller_properties() -> None:
     """Test that we can call the getters and setters."""
     turret_controller = TurretController()
 
-    assert turret_controller.x == 0
-    turret_controller.x = 90
-    assert turret_controller.x == 90
-    turret_controller.x = -90
-    assert turret_controller.x == -90
+    turret_controller.set_x(1)
+    turret_controller.set_x(-1)
 
-    assert turret_controller.y == 0
-    turret_controller.y = 90
-    assert turret_controller.y == 90
-    turret_controller.y = -10
-    assert turret_controller.y == -10
+    turret_controller.set_y(1)
+    turret_controller.set_y(0)
 
 
 def test_turret_controller_raises() -> None:
@@ -28,19 +22,26 @@ def test_turret_controller_raises() -> None:
     turret_controller = TurretController()
 
     with pytest.raises(RangeOfMovementError):
-        turret_controller.x = 91
+        turret_controller.set_x(1.1)
     with pytest.raises(RangeOfMovementError):
-        turret_controller.x = -91
+        turret_controller.set_x(-1.1)
 
     with pytest.raises(RangeOfMovementError):
-        turret_controller.y = 91
+        turret_controller.set_y(1.1)
     with pytest.raises(RangeOfMovementError):
-        turret_controller.y = -11
+        turret_controller.set_y(-0.1)
 
 
 @patch("sentrybot.turret_controller.Servo")
-def test_turret_controller_output_devices(pwm_device) -> None:
+def test_turret_controller_output_devices(servo: MagicMock) -> None:
     """Test that servos are activated as they should be."""
-    turret_controller = TurretController()
-    turret_controller.x = 90
-    assert pwm_device.return_value.value == 90
+    TurretController()
+
+    # More to do here, see
+    # https://gpiozero.readthedocs.io/en/stable/api_output.html?highlight=servo#gpiozero.Servo
+    servo.assert_has_calls(
+        [
+            call(),
+            call(),
+        ]
+    )
