714364d043443bd4d4080c1968ff4a4306f5a6ad
Iain <25081046+Iain-S@users.noreply.github.com>
2022-11-17 18:46:08 +0000

    Enable y-axis movement

--- sentrybot/turret_controller.py
+++ sentrybot/turret_controller.py
@@ -1,7 +1,8 @@
 """Module for moving, and streaming video from, the physical turret."""
 from time import sleep
 
-import gpiozero  # type: ignore
+from gpiozero import Servo  # type: ignore
+from gpiozero.pins.pigpio import PiGPIOFactory  # type: ignore
 
 
 class RangeOfMovementError(Exception):
@@ -12,15 +13,9 @@ class TurretController:
     """Controls for the physical turret."""
 
     def __init__(self) -> None:
-        self._x_servo = gpiozero.Servo(
-            13, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
-        )
-        self._y_servo = gpiozero.Servo(
-            12, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
-        )
-        self._breach_servo = gpiozero.Servo(
-            26, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
-        )
+        self._x_servo = Servo(13, pin_factory=PiGPIOFactory())
+        self._y_servo = Servo(12, pin_factory=PiGPIOFactory())
+        self._breach_servo = Servo(26, pin_factory=PiGPIOFactory())
 
     def set_x(self, value: float) -> None:
         """Set the current objective x value."""
@@ -28,7 +23,7 @@ class TurretController:
         # if not -1 <= value <= 1:
         if not -0.5 <= value <= 0.5:
             raise RangeOfMovementError
-        self._x_servo.value = 1
+        self._x_servo.value = value
 
     def set_y(self, value: float) -> None:
         """Set the current objective y value."""
@@ -36,6 +31,8 @@ class TurretController:
         # if not -1 <= value <= 1:
         if not -0.5 <= value <= 0.5:
             raise RangeOfMovementError
+
+        self._y_servo.value = value
 
     def launch(self) -> None:
         """Blocks until the turret has fired."""
