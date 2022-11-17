af436ca12b57cf7f15f7d00d0a716c87ea71e8d5
Iain <25081046+Iain-S@users.noreply.github.com>
2022-11-17 18:24:41 +0000

    Add launch() method

--- sentrybot/turret_controller.py
+++ sentrybot/turret_controller.py
@@ -1,5 +1,7 @@
 """Module for moving, and streaming video from, the physical turret."""
-from gpiozero import Servo
+from time import sleep
+
+import gpiozero
 
 
 class RangeOfMovementError(Exception):
@@ -10,16 +12,37 @@ class TurretController:
     """Controls for the physical turret."""
 
     def __init__(self) -> None:
-        self._x_servo = Servo()
-        self._y_servo = Servo()
+        self._x_servo = gpiozero.Servo(
+            13, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
+        )
+        self._y_servo = gpiozero.Servo(
+            12, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
+        )
+        self._breach_servo = gpiozero.Servo(
+            26, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
+        )
 
     def set_x(self, value: float) -> None:
         """Set the current objective x value."""
-        if not -1 <= value <= 1:
+
+        # if not -1 <= value <= 1:
+        if not -0.5 <= value <= 0.5:
             raise RangeOfMovementError
         self._x_servo.value = 1
 
     def set_y(self, value: float) -> None:
         """Set the current objective y value."""
-        if not 0 <= value <= 1:
+
+        # if not -1 <= value <= 1:
+        if not -0.5 <= value <= 0.5:
             raise RangeOfMovementError
+
+    def launch(self) -> None:
+        """Blocks until the turret has fired."""
+
+        self._breach_servo.value = -0.5
+
+        # ToDo Is this necessary?
+        sleep(0.01)
+
+        self._breach_servo.value = 0
--- stubs/gpiozero/__init__.pyi
+++ stubs/gpiozero/__init__.pyi
@@ -1,5 +1,10 @@
 """Mypy type stubs for the gpiozero package."""
+from typing import Optional
+from pins.pi import PiFactory
+
 
 
 class Servo:
     value: float
+    def __init__(self, pin_factory: Optional[PiFactory]=None) -> None:
+        ...
