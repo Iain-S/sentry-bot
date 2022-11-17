f141221ddf04a31f8b806cd44488c6412990b253
Iain <25081046+Iain-S@users.noreply.github.com>
2022-11-17 18:46:08 +0000

    Enable y-axis movement

--- tests/test_turret_controller.py
+++ tests/test_turret_controller.py
@@ -1,6 +1,6 @@
 """Tests for the turret_controller module.
 
-Remember to patch all gpiozero imports on every test function.
+Remember to patch gpiozero imports on every test function.
 Otherwise, it will complain that it cannot find the Raspberry Pi hardware."""
 from unittest.mock import MagicMock, call, patch
 
@@ -10,7 +10,8 @@ from sentrybot.turret_controller import RangeOfMovementError, TurretController
 
 
 @patch("sentrybot.turret_controller.Servo")
-def test_turret_controller_properties(_: MagicMock) -> None:
+@patch("sentrybot.turret_controller.PiGPIOFactory")
+def test_turret_controller_properties(_: MagicMock, __: MagicMock) -> None:
     """Test that we can call the getters and setters."""
     turret_controller = TurretController()
 
@@ -22,7 +23,8 @@ def test_turret_controller_properties(_: MagicMock) -> None:
 
 
 @patch("sentrybot.turret_controller.Servo")
-def test_turret_controller_raises(_: MagicMock) -> None:
+@patch("sentrybot.turret_controller.PiGPIOFactory")
+def test_turret_controller_raises(_: MagicMock, __: MagicMock) -> None:
     """Test that errors are raised for invalid angles."""
     turret_controller = TurretController()
 
@@ -38,7 +40,8 @@ def test_turret_controller_raises(_: MagicMock) -> None:
 
 
 @patch("sentrybot.turret_controller.Servo")
-def test_turret_controller_output_devices(servo: MagicMock) -> None:
+@patch("sentrybot.turret_controller.PiGPIOFactory")
+def test_turret_controller_output_devices(servo: MagicMock, __: MagicMock) -> None:
     """Test that servos are activated as they should be."""
     mock_x, mock_y = MagicMock(), MagicMock()
     servo.side_effect = [mock_x, mock_y]
@@ -48,9 +51,8 @@ def test_turret_controller_output_devices(servo: MagicMock) -> None:
     # https://gpiozero.readthedocs.io/en/stable/api_output.html?highlight=servo#gpiozero.Servo
     servo.assert_has_calls([call(), call()])
 
-    turret_controller.set_x(1)
-    assert mock_x.value == 1
+    turret_controller.set_x(0.1)
+    assert mock_x.value == 0.1
 
-    # ToDo As for x
-    turret_controller.set_y(1)
-    assert mock_y.value == 1
+    turret_controller.set_y(0.1)
+    assert mock_y.value == 0.1
