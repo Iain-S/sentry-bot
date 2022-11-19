87ca3c38b8254b342b0ea52e77e8255c7adfbbd1
Iain <25081046+Iain-S@users.noreply.github.com>
2022-11-19 12:20:22 +0000

    Fix pre-commit errors

--- sentrybot/video_picamera.py
+++ sentrybot/video_picamera.py
@@ -1,25 +1,32 @@
 """Generators for video streams that use a Raspberry Pi camera."""
-import os
-from typing import Generator, List
-import picamera
 import io
+import os
 from threading import Condition
+from typing import Final, Generator, List
 
-RESOLUTION = os.environ.get('RESOLUTION', '640x480')
-FRAMERATE = int(os.environ.get('FRAMERATE', 24))
-ROTATION = int(os.environ.get('ROTATION', 0))
+# pylint: disable=import-error
+import picamera  # type: ignore
 
-if ROTATION not in [0, 90, 180, 270]:
-    ROTATION = 0
+# pylint: enable=import-error
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
+RESOLUTION: Final[str] = os.environ.get("RESOLUTION", "640x480")
+FRAMERATE: Final[int] = int(os.environ.get("FRAMERATE", 24))
+ROTATION: Final[int] = int(os.environ.get("ROTATION", 0))
+
+assert ROTATION in [0, 90, 180, 270]
+
+
+class StreamingOutput:
+    """todo."""
+
+    def __init__(self) -> None:
+        self.frame: bytes = bytes()
         self.buffer = io.BytesIO()
         self.condition = Condition()
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
+    def write(self, buf: bytes) -> int:
+        """todo."""
+        if buf.startswith(b"\xff\xd8"):
             # New frame, copy the existing buffer's content and notify all
             # clients it's available
             self.buffer.truncate()
@@ -29,7 +36,7 @@ class StreamingOutput(object):
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
- 
+
 def generate_file_video(video_path: str) -> Generator[bytes, None, None]:
     """Generate a video stream from a file."""
     del video_path
@@ -40,26 +47,25 @@ def generate_camera_video(
     mouse_position: List[int],
 ) -> Generator[bytes, None, None]:
     """Generate a video stream from a Raspberry Pi camera."""
-    with picamera.PiCamera(resolution='%s' % RESOLUTION,
-                       framerate=FRAMERATE) as camera:
+    with picamera.PiCamera(resolution=f"{RESOLUTION}", framerate=FRAMERATE) as camera:
         camera.rotation = ROTATION
-        camera.annotate_background = picamera.Color('black')
-        #camera.annotate_text = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%SZ')
+        camera.annotate_background = picamera.Color("black")
 
         output = StreamingOutput()
-        camera.start_recording(output, format='mjpeg')
+        camera.start_recording(output, format="mjpeg")
 
         while True:
-            #self.server.camera.annotate_text = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%SZ')
             with output.condition:
                 output.condition.wait()
                 frame = output.frame
-            #self.wfile.write(b'--FRAME\r\n')
-            #self.send_header('Content-Type', 'image/jpeg')
-            #self.send_header('Content-Length', len(frame))
-            #self.end_headers()
-            #self.wfile.write(frame)
-            #self.wfile.write(b'\r\n')
-            yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + bytearray(frame) + b"\r\n"
+            # self.wfile.write(b'--FRAME\r\n')
+            # self.send_header('Content-Type', 'image/jpeg')
+            # self.send_header('Content-Length', len(frame))
+            # self.end_headers()
+            # self.wfile.write(frame)
+            # self.wfile.write(b'\r\n')
+            yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + bytearray(
+                frame
+            ) + b"\r\n"
 
     del mouse_position
