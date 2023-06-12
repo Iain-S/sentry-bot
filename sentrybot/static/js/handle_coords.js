c6b3be9deed81efae8274be99e8be7e628b26e99
joseph-palmer <22678593+joseph-palmer@users.noreply.github.com>
2023-06-12 16:38:42 +0100

    Adds fire button

--- sentrybot/static/js/handle_coords.js
+++ sentrybot/static/js/handle_coords.js
@@ -9,3 +9,16 @@ function testFunc() {
     .then(response => response.text())
     .then(text => console.log(text))
 }
+
+function fire() {
+    xpos = document.getElementById("xpos").value
+    ypos = document.getElementById("ypos").value
+    var params = new URLSearchParams();
+    params.append('xPos', xpos);
+    params.append('yPos', ypos);
+    params.append('shouldFire', true)
+    var url = '/set_desired_coords' + params.toString();
+    fetch(url)
+    .then(response => response.text())
+    .then(text => console.log(text))
+}
