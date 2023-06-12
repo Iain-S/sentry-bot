63b8f992d30ed6f6a3ce8cb18c8b46aa0e73d907
joseph-palmer <22678593+joseph-palmer@users.noreply.github.com>
2023-06-12 15:56:47 +0100

    changed label

--- sentrybot/static/js/handle_coords.js
+++ sentrybot/static/js/handle_coords.js
@@ -4,7 +4,7 @@ function testFunc() {
     var params = new URLSearchParams();
     params.append('xPos', xpos);
     params.append('yPos', ypos);
-    var url = '/set_desired_coords?' + params.toString();
+    var url = '/set_desired_coords' + params.toString();
     fetch(url)
     .then(response => response.text())
     .then(text => console.log(text))
