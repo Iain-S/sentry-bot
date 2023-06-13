a85bca05cd4a6432a5acf912dd23aa5e107b8ccd
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-06-13 11:02:53 +0100

    Add pylint ignores

--- sentrybot/video_opencv.py
+++ sentrybot/video_opencv.py
@@ -15,6 +15,8 @@ from sentrybot.client_instruction import ClientInstruction
 from sentrybot.http_server import StreamingOutput
 from sentrybot.turret_controller import TurretController
 
+# pylint: disable=fixme,unused-argument
+
 
 def generate_file_video(video_path: str) -> Generator[bytes, None, None]:
     """Generate a video stream from a file."""
