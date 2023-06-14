d80cec5feb175fd36b0324e46529247c2d64a1b2
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2023-06-14 13:34:18 +0100

    Making a bigger offset

--- sentrybot/video_opencv.py
+++ sentrybot/video_opencv.py
@@ -185,7 +185,7 @@ def _aim(
     default_nudge: float = Settings().default_nudge
     if current_distance <= firing_threshold:
         if turret_controller:
-            turret_controller.nudge_y(-default_nudge * 2)
+            turret_controller.nudge_y(default_nudge * 2)
             _add_text(streaming_frame, "FIRE!!!!")
             turret_controller.launch()
     elif current_center_x < image_center_x and x_distance > firing_threshold:
