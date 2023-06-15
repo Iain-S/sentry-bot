7d79c3e5117c66c034a693b06bfc8f68174c448f
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2023-06-15 13:22:06 +0100

    Defining parameters in file

--- sentrybot/settings.py
+++ sentrybot/settings.py
@@ -17,14 +17,14 @@ class Settings(BaseSettings):
     camera_library: CameraLibrary
     control_turret: bool  # Try to move the turret
     do_aiming: bool  # Enable for auto-aiming.
-    minimum_hue_target: int = 0
+    minimum_hue_target: int = 0  # For the tennis ball, keep hue between 0 and 60.
     maximum_hue_target: int = 60
-    minimum_value_target: int = 0
+    minimum_value_target: int = 0  # For the cat, values should be between 0 and 30
     maximum_value_target: int = 255
     streaming_source: int = 2  # 0 for raw camera output
     do_haar_aiming: bool = False
-    firing_threshold: int = 20  # 30 for half-distance targets
+    firing_threshold: int = 20  # 10+ for half-distance targets
     frame_delay: float = 0.03
-    default_nudge: float = 0.1  # 0.05 for fine-tuning aiming.
+    default_nudge: float = 0.1  # 0.04 for fine-tuning aiming.
     rotate_feed: bool = True  # Rotate the camera feed
-    camera_offset: int = 100
+    camera_offset: int = 100  # 175 for mid distance balls
